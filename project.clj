9fc5dd43cf95f571e7e9976567ccf051de0b7a23
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2011-12-12 14:34:30 +0100

    [internal] use H2 as development base, do not require it at runtime

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject mbs-db "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
+  :dev-dependencies [[com.h2database/h2 "1.3.162"]]
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/java.jdbc "0.1.0"]
                  [org.clojure/data.json "0.1.2"]
                  [clj-cache "0.0.4"]
                  ;; TODO use either or, or make configurable by consumer application
-                 [com.h2database/h2 "1.3.162"]
                  [mysql/mysql-connector-java "5.1.17"]])
--- test/mbs_db/test/core.clj
+++ test/mbs_db/test/core.clj
@@ -1,6 +1,20 @@
 (ns mbs-db.test.core
-  (:use [mbs-db.core])
+  (:require [mbs-db.core :as db])
   (:use [clojure.test]))
 
 (deftest replace-me ;; FIXME: write
   (is false "No tests have been written."))
+
+(comment
+  
+  (binding [mbs-db.core/*db* {:classname   "org.h2.Driver"
+              :subprotocol "h2"
+              :user        "sa"
+              :password     ""
+              :subname      "~/solarlog"}]
+    (db/count-all-values))
+  )
+
+;;create table ts2 (time timestamp not null, value int, belongs int);
+;;create table tsnames(name varchar(255), belongs int);
+;;create table metadatajson(name varchar(255), json varchar(30000));
