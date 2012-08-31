fa81476fe412826464da1cd72dd39d3e27c2f622
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2012-08-31 17:24:34 +0200

    [refactor] use common database schema for siemens, psm, etc.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mbs-db "1.0.0-SNAPSHOT"
+(defproject mbs-db "1.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :dev-dependencies [[com.h2database/h2 "1.3.162"]]
   :dependencies [[org.clojure/clojure "1.3.0"]
--- src/mbs_db/core.clj
+++ src/mbs_db/core.clj
@@ -1,26 +1,69 @@
 (ns mbs-db.core
   (:use 
-    [mbs-db.util :only (encrypt-name encrypt decrypt-name)]
     [org.clojars.smee 
      [map :only (map-values)]
      [time :only (as-unix-timestamp as-sql-timestamp)]])
   (:require 
     [clojure.java.jdbc :as sql]
-    [clojure.core.memoize :as cache])
-  )
+    [clojure.core.memoize :as cache]))
 
-(def h2-config {:classname   "org.h2.Driver"
-                :subprotocol "h2"
-                :user        "sa"
-                :password     ""
-                :subname      "~/solarlog"})
-(def mysql-config {:classname   "com.mysql.jdbc.Driver"
-                   :subprotocol "mysql"
-                   :user        "read"
-                   :password     "eumpw"
-                   :subname      "//localhost:5029/solarlog"})
-(def ^:dynamic *db* mysql-config)
+(def mysql-config-default {:classname   "com.mysql.jdbc.Driver"
+                           :subprotocol "mysql"
+                           :user        "root"
+                           :password     ""
+                           :subname      "//localhost:5029/siemens"})
 
+(def mysql-config-siemens (assoc mysql-config-default 
+                                 :subname "//localhost:5029/siemens"
+                                 :password (get (System/getenv) "eumdb01_password") ))
+(def mysql-config-psm (assoc mysql-config-default :subname "//localhost:5029/psm"))
+
+(def ^:dynamic *db* mysql-config-psm)
+
+(defn create-tables-siemens []
+  (sql/with-connection *db*
+      (sql/create-table :series_data
+                        [:plant "varchar(255)" "comment 'lookup'"] ; use infobright's lookup feature for better compression
+                        [:name "varchar(255)" "comment 'lookup'"] ; use infobright's lookup feature for better compression
+                        [:timestamp "timestamp"] 
+                        [:value "double"] 
+                        [:quality "int"] 
+                        [:flags "int"])
+      (sql/create-table :plant
+                        [:name"varchar(127)"]
+                        [:street "varchar(127)"]
+                        [:street_number "varchar(127)"]
+                        [:zipcode "varchar(127)"]
+                        [:city "varchar(127)"]
+                        [:country"varchar(127)"])
+      (sql/create-table :customer
+                        [:name"varchar(127)"]
+                        [:street "varchar(127)"]
+                        [:street_number "varchar(127)"]
+                        [:zipcode "varchar(127)"]
+                        [:city "varchar(127)"]
+                        [:country"varchar(127)"]) 
+      (sql/create-table :series 
+                        [:plant "varchar(127)"] ;name of the power plant
+                        [:name "varchar(127)"] ;name of the series 
+                        [:identification "varchar(127)"]; ???
+                        [:unit "varchar(127)"] ;physical SI unit
+                        [:scale "varchar(127)"] ;???
+                        [:component "varchar(127)"] ;name of the origin component
+                        [:type "varchar(127)"] ;type of the series
+                        [:resolution "int"]) ;average time between two measures in milliseconds
+      ))
+
+(defn import-into-infobright* [table & data-csv-files]
+  (sql/with-connection 
+    *db*
+    (apply sql/do-commands 
+      "set @bh_dataformat = 'txt_variable'"
+      (for [file data-csv-files] 
+        (format "load data infile '%s' into table %s"  (.replaceAll (str file) "\\\\" "/") table)))))
+
+(defn import-into-infobright [& data-csv-files]
+  (apply import-into-infobright* "series_data" data-csv-files))
 
 
 (defn adhoc [query & params]
@@ -45,82 +88,46 @@ sequence of results by manipulating the var 'res'. Handles name obfuscation tran
      (alter-var-root #'~name cache/memo-lru ~num-to-cache)))
 
 (defn- fix-time
-  ([r] (fix-time r :time))
+  ([r] (fix-time r :timestamp))
   ([r & keys]
   (reduce #(if-let [ts (get % %2)] 
              (assoc % %2 (as-unix-timestamp ts))
              (assoc % %2 0)) 
           r keys)))
 
-(defn create-tables []
-  (sql/with-connection *db*
-      (sql/create-table :ts2 [:belongs "int"] [:value "int"] [:timestamp "timestamp"])
-      (sql/create-table :tsnames [:name "varchar(255)"] [:belongs "int"])
-      (sql/create-table :metadatadetails 
-                        [:AnlagenKWP "int"]
-                        [:AnzahlWR "int"]
-                        [:BannerZeile1 "varchar(255)"]
-                        [:BannerZeile2 "varchar(255)"]
-                        [:BannerZeile3 "varchar(255)"]
-                        [:HPAusricht "varchar(255)"]
-                        [:HPBetreiber "varchar(255)"]
-                        [:HPEmail "varchar(255)"]
-                        [:HPInbetrieb "varchar(255)"]
-                        [:HPLeistung "varchar(255)"]
-                        [:HPModul "varchar(255)"]
-                        [:HPPostleitzahl "char(5)"]
-                        [:HPStandort "varchar(255)"]
-                        [:HPTitel "varchar(255)"]
-                        [:HPWR "varchar(255)"]
-                        [:Serialnr "varchar(255)"]
-                        [:SollYearKWP "int"]
-                        [:Verguetung "int"]
-                        [:id "varchar(255)"])))
+;;;;;;;;; series meta data ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defquery count-all-values "Count all values" 
-  "select count(*) as num from ts2"
-  (:num (first res)))
+(defquery count-all-series-of-plant "Count all time series where the name of the plant is the given parameter"
+  "select count(*) as num from series where plant= ?;"
+  (apply + (map :num res)))
 
-(defquery-cached all-names-limit 20000 "Select all pv names (first part of the time series' names)."
-  "select distinct SUBSTRING_INDEX(name,'.',1) as name from tsnames limit ?,?"
-  (doall (map :name res)))
+(defquery all-series-names-of-plant "Select all time series names with given plant name"
+  "select identification from series where plant=?;"
+  (doall (map :identification res)))
 
-(defquery-cached count-all-series 1 "Count all available time series."
-  "select count(*) as num from tsnames;"
-  (:num (first res)))
+;;;;;;;;; time series values ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defquery count-all-series-of "Count all time series where the name is like the given parameter"
-  "select count(name) as num from tsnames where name like ?"
-  (:num (first res)))
+;(defquery all-values-of "Select all time series data points of a given name."
+;  "select time, value from series_data where belongs=(select belongs from tsnames where name=?)  order by time"
+;  (doall (map fix-time res)))
 
-(defquery all-series-names-of "Select all time series names that are like the given parameter"
-  "select * from tsnames where name like ?  order by name"
-  (doall (map :name res)))
-
-(defquery count-all-values-of "Count all time series data points where the name is like the given parameter"
-  "select count(*) as num from ts2 where belongs=(select belongs from tsnames where name=?)"
-  (:num (first res)))
-
-(defquery all-values-of "Select all time series data points of a given name."
-  "select time, value from ts2 where belongs=(select belongs from tsnames where name=?)  order by time"
+(defquery-cached all-values-in-time-range 30 "Select all time series data points of a given plant and series id that are between two times."
+  "select timestamp, value from series_data where plant=? and name=? and timestamp >? and timestamp <?  order by timestamp"
   (doall (map fix-time res)))
 
-(defquery-cached all-values-in-time-range 30 "Select all time series data points of a given name that are between two times."
-  "select time, value from ts2 where belongs=(select belongs from tsnames where name=?) and time >? and time <?  order by time"
-  (doall (map fix-time res)))
-
-(defquery-cached min-max-time-of 10000 "Select time of the oldest data point of a time series."
-  "select min(time) as min, max(time) as max from ts2 where belongs=(select belongs from tsnames where name=?)"
+(defquery-cached min-max-time-of 10000 "Select time of the oldest/newest data point of a time series."
+  "select min(timestamp) as min, max(timestamp) as max from series_data where plant=? and name=?"
   (fix-time (first res) :min :max))
 
+;todo
 (defquery summed-values-in-time-range "Select times and added values of all time series that match a given parameter and are between two times."
   "select time, sum(value) as value 
-     from ts2 where belongs in (select belongs from tsnames where name like ?)  
+     from series_data where belongs in (select belongs from tsnames where name like ?)  
       and time>? and time<? 
  group by time 
  order by time"
   (doall (doall (map (comp #(assoc % :value (.doubleValue (:value %))) fix-time) res))))
-
+;todo
 (defn get-efficiency [id wr-id start end]
   (let [[pdc-sum pac] (pvalues
                      (summed-values-in-time-range (format "%s.wr.%s.pdc.string.%%" id wr-id) (as-sql-timestamp start) (as-sql-timestamp end))
@@ -129,16 +136,18 @@ sequence of results by manipulating the var 'res'. Handles name obfuscation tran
                         (map :value pac) (map :value pdc-sum))]
     (map #(hash-map :time % :value %2) (map :time pac) efficiency)))
 (alter-var-root #'get-efficiency cache/memo-lru 1000)
-
+;todo
 ;; TODO more generic? allow all kinds of time intervals, days, weeks, months, years....
-(def ^:private daily "select  sum(maxes.value) as value, maxes.t as time from
-     (select max(value) as value, date(time) as t, belongs 
-      from ts2 
-      where belongs in (select belongs from tsnames where name like ?) 
-      and time>? and time<?
-      group by t, belongs) as maxes
-   group by maxes.t order by time")
 
+(def ^:private daily 
+           "select sum(value) as value, maxima.t as time from
+              (select date(timestamp) as t, max(value) as value, name from series_data 
+               where name like 'INVU%/DAY_MMTR0%'
+                     and plant=?
+                     and timestamp>? and timestamp<? 
+               group by t, name) as maxima
+            group by maxima.t order by t")
+;todo
 (defquery-cached sum-per-day 10 "Select sum of gains per day of a series in a time interval"
   daily
   (doall (map fix-time res)))
@@ -156,60 +165,16 @@ sequence of results by manipulating the var 'res'. Handles name obfuscation tran
 (defn get-metadata "get map of metadata for multiple pv installations in one query." 
   [& names]
   (let [query (if names 
-                (apply str "select * from metadatadetails where id=?" (repeat (dec (count names)) " or id=?"))
-                "select * from metadatadetails")] 
-    ;; run the query
-    (sql/with-connection 
-      *db* 
-      (sql/with-query-results 
-        res 
-        (reduce conj [query] names)
-        (let [;; metadata has wrong encoding, reinterpret every string as latin1 :(
-              fixed-utf8 (for [r res] (map-values #(if (string? %) (String. (.getBytes % "latin1")) %) r))
-              fixed-income (for [r fixed-utf8] (update-in r [:verguetung] #(double (/ % 100))))
-              ;; censor private data
-              private-names [:bannerzeile1 :bannerzeile2 :bannerzeile3 :hpbetreiber :hpemail :hpstandort :hptitel]
-              encrypted (for [r fixed-income] 
-                          (reduce #(update-in % [%2] encrypt) r private-names))]
-          (zipmap (map :id encrypted) encrypted))))))
-(alter-var-root #'get-metadata cache/memo-lru 1000)
+                (apply str "select * from plant where name=?" (repeat (dec (count names)) " or name=?"))
+                "select * from plant")] 
+    ; TODO need real metadata, number of inverters etc.
+    (sql/with-connection *db* 
+       (sql/with-query-results res (reduce conj [query] names)
+            (zipmap (map :name res) (map #(hash-map :address % :anzahlwr 2 :anlagenkwp 1000000) res))))))
+(alter-var-root #'get-metadata cache/memo-lru 100)
 
-(comment
-  
-  (binding [*db* *db*]
-    (let [df (java.text.SimpleDateFormat. "yyyy-MM-dd")
-          start (.. df (parse "2011-01-01") getTime)
-          end (.. df (parse "2011-12-31") getTime)] 
-      #_(num-series db)
-      #_(all-names 0 10)
-      #_(time (def x (all-values-of "1468.wr.0.pdc.string.0")))
-      #_(time (def x (all-values-in-time-range "1468.wr.0.pdc.string.0" (java.sql.Timestamp. start) (java.sql.Timestamp. end))))
-      (count-all-series-of "1555"))
-    )
-  
-  )
-
-(comment
-  
-  (use 'clojure.java.io)
-  (use '[clojure.string :only (split)])
-  (use '[org.clojars.smee.util :only (s2f)])
-  (def series (->> "d:\\Dropbox\\Arbeit\\Projekte\\EUMONIS\\Usecase PSM Solar\\Daten\\average minute INVU1 2012_02_05.csv" file reader line-seq (map #(split % #";"))))
-  (use '[incanter [core :only (view)] charts])
-  (def s2
-    (let [df (java.text.SimpleDateFormat. "dd.MM.yyyy HH:mm")]
-      (cons (first series) (map #(update-in % [0] (fn [d] (.getTime (.parse df d)))) (rest series)))))
-  (def s3 (vec (apply map vector s2)))
-  
-  (let [times (rest (first s3))
-        series (rest s3)
-        names (map first series)
-        nf (java.text.NumberFormat/getInstance java.util.Locale/GERMANY )
-        series (map (fn [s] (map #(.parse nf %) (rest s))) series)
-        c (time-series-plot [] [] :legend true)
-        series (next series)
-        names (next names)]
-    (doseq [[n s] (map list names series) :when (.contains (.toLowerCase n) "apparent")]
-      (add-lines c times s :series-label n)) 
-    (view c))
-  )
+;;;;;;;; internal statistics ;;;;;;;;;;;;;;;;;;;;
+(defn data-base-statistics []
+  (adhoc "SELECT table_schema, sum( data_length + index_length ) / 1024 / 1024 'Data Base Size in MB',TABLE_COMMENT FROM information_schema.TABLES WHERE ENGINE = 'BRIGHTHOUSE' GROUP BY table_schema"))
+(defn table-statistics []
+  (adhoc "SHOW TABLE STATUS WHERE ENGINE='BRIGHTHOUSE'"))
