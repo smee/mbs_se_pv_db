747a20680593636000ad2b8359a6bca5c546155f
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2012-09-21 10:21:28 +0200

    [build] remove unused json dep

--- project.clj
+++ project.clj
@@ -3,6 +3,5 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/java.jdbc "0.1.0"]
                  [c3p0/c3p0 "0.9.1.2"]
-                 [org.clojure/data.json "0.1.2"]
                  [org.clojure/core.memoize "0.5.1"]
                  [mysql/mysql-connector-java "5.1.21"]])
