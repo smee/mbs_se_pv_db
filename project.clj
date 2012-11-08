c48ca8fadc9df30da383c0830011b12735700ef1
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2012-11-08 14:23:25 +0100

    [internal] update jdbc dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject mbs-db "1.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/java.jdbc "0.1.0"]
+                 [org.clojure/java.jdbc "0.2.3"]
                  [c3p0/c3p0 "0.9.1.2"]
                  [org.clojure/core.memoize "0.5.1"]
                  [mysql/mysql-connector-java "5.1.21"]])
