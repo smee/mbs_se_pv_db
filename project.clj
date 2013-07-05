f01109265d5617f8ef78cc78e5582a541d76d2f6
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2013-07-05 11:10:55 +0200

    bump versions

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [c3p0/c3p0 "0.9.1.2"]
-                 [org.clojure/core.memoize "0.5.1"]
-                 [mysql/mysql-connector-java "5.1.21"]])
+                 [org.clojure/core.memoize "0.5.6"]
+                 [mysql/mysql-connector-java "5.1.25"]])
